b5bbcdb24e90e421514b6ea0ec499211394f808e
Anshu <riyanshu062@gmail.com>
2026-01-13 12:19:41 +0530

    Footer part added

--- client/src/components/educator/Footer.jsx
+++ client/src/components/educator/Footer.jsx
@@ -1,10 +1,53 @@
 import React from "react";
+import { assets } from "../../assets/assets";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Edu Footer</h1>
-    </div>
+    <footer className="w-full border-t bg-white">
+      <div
+        className="flex flex-col md:flex-row 
+        items-center justify-between 
+        text-left w-full px-8 md:px-20 py-5"
+      >
+        {/* Left */}
+        <div className="flex items-center gap-3">
+          <img className="hidden md:block w-20" src={assets.logo} alt="logo" />
+
+          <div className="hidden md:block h-7 w-px bg-gray-300"></div>
+
+          <p className="text-xs md:text-sm text-gray-500">
+            © {new Date().getFullYear()} LMS Project. All Rights Reserved.
+          </p>
+        </div>
+
+        {/* Right - Social */}
+        <div className="flex items-center gap-4 mt-4 md:mt-0">
+          <a href="#">
+            <img
+              src={assets.facebook_icon}
+              alt="facebook"
+              className="w-5 hover:scale-110 transition"
+            />
+          </a>
+
+          <a href="#">
+            <img
+              src={assets.twitter_icon}
+              alt="twitter"
+              className="w-5 hover:scale-110 transition"
+            />
+          </a>
+
+          <a href="#">
+            <img
+              src={assets.instagram_icon}
+              alt="instagram"
+              className="w-5 hover:scale-110 transition"
+            />
+          </a>
+        </div>
+      </div>
+    </footer>
   );
 };
 
--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/educator/Navbar";
+import Sidebar from "../../components/educator/Sidebar";
+import Footer from "../../components/educator/Footer";
 
 const Educator = () => {
   return (
-    <div className="text-default min-h-screen bg-white">
+    <div className="text-default min-h-screen bg-gray-50 flex flex-col">
+      {/* Top Navbar */}
       <Navbar />
-      <h1>Educator Pages</h1>
-      {<Outlet />}
+
+      {/* Body */}
+      <div className="flex flex-1">
+        {/* Sidebar */}
+        <Sidebar />
+
+        {/* Main Content */}
+        <main className="flex-1 p-6 bg-white">
+          <Outlet />
+        </main>
+      </div>
+
+      {/* Footer */}
+      <Footer />
     </div>
   );
 };
