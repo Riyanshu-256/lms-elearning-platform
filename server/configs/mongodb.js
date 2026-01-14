0e06ea21c4ca4f297d372f0a71d1c3b3eb16cf19
Anshu <riyanshu062@gmail.com>
2026-01-14 16:22:21 +0530

    fixes the bug

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -2,8 +2,16 @@ import mongoose from "mongoose";
 
 // Connect to the MongoDB database
 const connectDB = async () => {
-  mongoose.connection.on("connected", () => console.log("Database Connected"));
+  try {
+    mongoose.connection.on("connected", () =>
+      console.log("Database Connected Successfully")
+    );
 
-  await mongoose.connect(`${process.env.MONGODB_URI}/skillforge`);
+    // Sirf URI use karo, extra /dbname mat lagao
+    await mongoose.connect(process.env.MONGODB_URI);
+  } catch (error) {
+    console.log("DB Connection Error:", error);
+  }
 };
+
 export default connectDB;
